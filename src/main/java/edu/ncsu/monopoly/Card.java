62e3954bac72321fed81bd70bbb5ab663540cb95
iturrioz <iturrioz@192.168.1.40>
2019-11-28 18:05:24 +0100

    Card final attributes

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -2,8 +2,8 @@ package edu.ncsu.monopoly;
 
 public abstract class Card {
 
-    public static int TYPE_CHANCE = 1;
-    public static int TYPE_CC = 2;
+    public static final int TYPE_CHANCE = 1;
+    public static final int TYPE_CC = 2;
 
     public abstract String getLabel();
     public abstract void applyAction();
